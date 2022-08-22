5e5767ef4d33b007f900cc0a24c86a08b8d3f17a
Sun1d0l <100133749+Sun1d0l@users.noreply.github.com>
2022-08-22 19:13:08 +0100

    Create GameObject.js
    Started to add the game object class

---
+++ GameObject.js
@@ -0,0 +1,14 @@
+class GameObject
+{
+    constructor(width, height, color, x, y, type)
+    {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.x = x;
+        this.y;
+        this.type = type;
+    }
+
+
+}
