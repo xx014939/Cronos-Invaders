dba190d9d69224c969830a52a0b45ce08254da0f
xx014939 <57679526+xx014939@users.noreply.github.com>
2022-09-03 21:22:45 +0100

    Wallet login

---
+++ walletAuth.js
@@ -0,0 +1,25 @@
+const serverUrl = "https://6hs6vzjfxpwd.usemoralis.com:2053/server";
+const appId = "8B9E4nYSiOlMo116PtN1JKmjA6QjuteqFTqWtxoC";
+Moralis.start({ serverUrl, appId });
+
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.authenticate({
+        signingMessage: "Log in using Moralis",
+      })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+}
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
+  
